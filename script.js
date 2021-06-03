56d53a66c22d80c24ce30c0c07ad3e4ce839f109
Paul Szadurski <paulszadurski2@gmail.com>
2021-06-03 17:01:40 -0400

    added a projects carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mobile nav
 const openMenu = () => {
   document.getElementById("mobileNav").style.top = "0";
 }
@@ -5,3 +6,50 @@ const openMenu = () => {
 const closeMenu = () => {
   document.getElementById("mobileNav").style.top = "-100%";
 }
+
+
+// project carousel
+let projectIndex = 1 
+
+const projectForward = (n) => {
+  showProject(projectIndex += n);
+}
+
+const showProject = (n) => {
+  let currentProject = document.getElementsByClassName("projectCarousel");
+  let testV = document.getElementById("projectNumber");
+
+
+  if (n > currentProject.length) {
+    projectIndex = 1
+  }
+
+  if (n < 1) {
+    projectIndex = 4;
+  }
+
+  for (i = 0; i < currentProject.length; i++) {
+    currentProject[i].style.display = "none";
+  }
+
+  currentProject[projectIndex - 1].style.display = "flex";
+
+  while (testV.hasChildNodes()) {
+    testV.removeChild(testV.firstChild);
+  }
+
+  let testN = document.createTextNode(projectIndex);
+
+  testV.appendChild(testN);
+}
+
+showProject(projectIndex);
+
+
+
+
+
+
+
+
+
